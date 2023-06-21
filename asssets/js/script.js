90476e88c726a88e6d94ee2f05ce8e73f8835309
codiologist <robin.promoteur@gmail.com>
2023-06-21 18:45:19 +0600

    Design and code Updated.

--- asssets/js/script.js
+++ asssets/js/script.js
@@ -4,6 +4,7 @@ const weatherAppWrapper = document.querySelector(".wrapper");
 const weatherCard = document.querySelector(".weather-card");
 const weatherCardBody = document.querySelector(".weather-card-body");
 const locationName = document.querySelector(".location-name h2");
+const locationTime = document.querySelector(".location-name h4");
 const currentTemperature = document.querySelector(".temperature .temp-current span");
 const maxTemperature = document.querySelector(".temperature .temp-max span");
 const minTemperature = document.querySelector(".temperature .temp-min span");
@@ -18,6 +19,7 @@ const APP_API_KEY = "f2051ed6dec700dcfb5c1bc40e50917c";
 
 const APP_API_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+//Disabale Search Button
 searchLocation.addEventListener("input", function () {
 	if (searchLocation.value === "") {
 		getWeatherStatusBtn.disabled = true;
@@ -26,6 +28,7 @@ searchLocation.addEventListener("input", function () {
 	}
 });
 
+//Get Weather Data Funcion From API
 const getWeatherData = async (city) => {
 	const response = await fetch(APP_API_URL + city + `&appid=${APP_API_KEY}`);
 	const data = await response.json();
@@ -35,9 +38,15 @@ const getWeatherData = async (city) => {
 	if (data.cod === "404") {
 		notFound.style.display = "block";
 		weatherCardBody.style.display = "none";
-		weatherCard.classList.remove("sunny-bg", "rain-bg", "cloud-bg", "haze-bg", "snow-bg");
+		weatherCard.classList.remove("sunny", "raining", "cloudy", "haze", "snow");
 		return;
 	}
+
+	const timezone = data.timezone; 
+	const timezoneInMinutes = timezone / 60;
+	const currTime = moment().utcOffset(timezoneInMinutes).format("h:mm A");
+	
+
 	notFound.style.display = "none";
 	weatherCardBody.style.display = "block";
 
@@ -49,56 +58,56 @@ const getWeatherData = async (city) => {
 	humidity.innerHTML = Math.round(data.main.humidity);
 	desc.innerHTML = data.weather[0].main;
 	windSpeed.innerHTML = Math.round(data.wind.speed);
+	locationTime.innerHTML = currTime
+	
 
+	//Changing icons and backgrounds logic
 	switch (data.weather[0].main) {
 		case "Clear":
 			weatherIcon.src = "/asssets/images/icons/sunny.svg";
-			weatherCard.classList.remove("rain-bg", "cloud-bg", "haze-bg", "snow-bg");
-			weatherCard.classList.add("sunny-bg");
+			weatherAppWrapper.classList.remove("raining", "cloudy", "haze", "snow");
+			weatherAppWrapper.classList.add("sunny");
 			break;
 
 		case "Rain":
 			weatherIcon.src = "/asssets/images/icons/rain.svg";
-			weatherCard.classList.remove("sunny-bg", "cloud-bg", "haze-bg", "snow-bg");
-			weatherCard.classList.add("rain-bg");
+			weatherAppWrapper.classList.remove("sunny", "cloudy", "haze", "snow");
+			weatherAppWrapper.classList.add("raining");
 			break;
 
 		case "Thunderstorm":
 			weatherIcon.src = "/asssets/images/icons/thunderstorms.svg";
-			weatherCard.classList.remove("sunny-bg", "cloud-bg", "haze-bg", "snow-bg");
-			weatherCard.classList.add("rain-bg");
+			weatherAppWrapper.classList.remove("sunny", "cloudy", "haze", "snow");
+			weatherAppWrapper.classList.add("raining");
 			break;
 
 		case "Drizzle":
 			weatherIcon.src = "/asssets/images/icons/moderate-rain.svg";
-			weatherCard.classList.remove("sunny-bg", "cloud-bg", "haze-bg", "snow-bg");
-			weatherCard.classList.add("rain-bg");
+			weatherAppWrapper.classList.remove("sunny", "cloudy", "haze", "snow");
+			weatherAppWrapper.classList.add("raining");
 			break;
 
 		case "Snow":
 			weatherIcon.src = "/asssets/images/icons/snow.svg";
-			weatherCard.classList.remove("sunny-bg", "cloud-bg", "haze-bg", "rain-bg");
-			weatherCard.classList.add("snow-bg");
+			weatherAppWrapper.classList.remove("sunny", "raining", "cloudy", "haze");
+			weatherAppWrapper.classList.add("snow");
 			break;
 
 		case "Haze":
 			weatherIcon.src = "/asssets/images/icons/haze.svg";
-			weatherCard.classList.remove("sunny-bg", "cloud-bg", "snow-bg", "rain-bg");
-			weatherCard.classList.add("haze-bg");
+			weatherAppWrapper.classList.remove("sunny", "raining", "cloudy", "snow");
+			weatherAppWrapper.classList.add("haze");
 			break;
 
 		case "Clouds":
 			weatherIcon.src = "/asssets/images/icons/cloudy.svg";
-			weatherCard.classList.remove("sunny-bg", "haze-bg", "snow-bg", "rain-bg");
-			weatherCard.classList.add("cloud-bg");
+			weatherAppWrapper.classList.remove("sunny", "raining", "haze", "snow");
+			weatherAppWrapper.classList.add("cloudy");
 			break;
-
-		default:
-			" ";
-			weatherCard.classList.remove("sunny-bg", "haze-bg", "snow-bg", "rain-bg", "cloud-bg");
 	}
 };
 
+// Calling Get Weather Data Funcion
 getWeatherStatusBtn.addEventListener("click", () => {
 	getWeatherData(searchLocation.value);
 });
